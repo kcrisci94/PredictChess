1ec46a76fbe1bc4c197fb968dc92b46831c03459
Kaleb Crisci <kcrisci94@gmail.com>
2021-02-18 20:37:18 -0800

    added mysql to server side and added request to api.chess.com to download all archive games from a user

--- server.js
+++ server.js
@@ -1,18 +1,50 @@
 const express = require('express');
-const cors = require('cors');
-
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 const app = express();
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-app.get('/api/customers', cors(), (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
+/*Use middleware to process JSON for POST requests*/
+app.use(bodyParser.json());
 
-  res.json(customers);
+/*Apply COORS access control headers*/
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", 
+      "Origin, X-Requested-With, Content-Type, Accept");
+   next();
 });
 
+/*Create connection to MySql database*/
+const db = mysql.createConnection({
+   host : 'localhost',
+   user : 'root',
+   password : '@WSX2wsx',
+   database : 'project'
+
+});
+
+db.connect((err) => {
+   if(err) throw err;
+});
+
+app.get('/games/mygames', (req, res) => {
+   const Http = new XMLHttpRequest();
+   const username = 'kcrisci';
+   const url="https://api.chess.com/pub/player/" + username + "/games/archives";
+   Http.open("GET", url);
+   Http.send();
+   Http.onreadystatechange = (e) => {
+      if(Http.readyState === 4) {
+         console.log("Pulled all Archives for user: ", username);
+         var json = JSON.parse(Http.responseText);
+         var results = json.results;
+         res.send(json);
+      }
+   }
+});
+
+//this is the port number specified in client .json file
 const port = 5000;
 
 app.listen(port, () => `Server running on port ${port}`);
