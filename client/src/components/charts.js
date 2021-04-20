7b4e31db5a82ac32eea9ba0ded858ac578618a31
Kaleb Crisci <kcrisci94@gmail.com>
2021-04-19 19:51:19 -0700

    added recommendation for chart1

--- client/src/components/charts.js
+++ client/src/components/charts.js
@@ -12,6 +12,18 @@ class Controls extends React.Component {
           options={options}
           // onRef = {ref => this.chart = ref}
         />
+        <div className="results">
+           <p>The Player's Top 3 Capturing Pieces are: 
+           <ul>
+               {topCaptures.map((item, index) => 
+                  <li key={index} onClick={() => {this.props.updateDisplay(item.added_user);}}>
+                     {item.label}: {item.y}
+                  </li>
+               )}       
+           </ul>
+           Try to take them out early, or block them with other pieces.
+           </p>
+        </div>
       </div>
      );
    }
